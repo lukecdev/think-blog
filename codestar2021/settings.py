16e0556b7e8c1f486c35398ab8d46bca535aa88a
lukecdev <courtney.luke@outlook.com>
2023-09-24 22:22:52 +0000

    adding summernote

--- codestar2021/settings.py
+++ codestar2021/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['thinkblog-1ba3cbdf5abc.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['thinkblog-1ba3cbdf5abc.herokuapp.com', 'localhost', '8000-lukecdev-thinkblog-k3t4b6j0zkj.ws-eu104.gitpod.io']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
